4f622682c2840e89dd843863aa293b78633c6f97
sugawara-ryota <sugawara_ryota@cyberagent.co.jp>
2018-10-09 10:25:47 +0900

    Add JavaScript file

---
+++ JavaScript/GitPushHackathon.js
@@ -0,0 +1,16 @@
+class GitPushHackathon {
+  constructor() {
+    console.log("Challenge!!");
+  }
+
+  win() {
+    return "Get 💴💴💴!";
+  }
+}
+
+const gitPushHackathon = new GitPushHackathon();
+
+const push = () => {
+  console.log(gitPushHackathon.win());
+};
+
